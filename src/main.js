7c31d48f1143df6641ea7d6986243f7018934173
Roberto Saliola <roberto.saliola96@gmail.com>
2026-03-15 12:20:56 +0100

    made the repo a library

--- src/main.js
+++ src/main.js
@@ -1,16 +1,1 @@
-import { getServer } from "./lib/network/server.js";
-import { PORT } from "../config.js";
-import "./api/books/routes.js";
-import { initDatabase } from "./lib/database/database.js";
-console.log(
-  (eval("var __temp = null"), typeof __temp === "undefined")
-    ? "strict"
-    : "non-strict"
-);
-
-const server = getServer();
-initDatabase();
-
-server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+export { getServer } from "./lib/network/server.js";
