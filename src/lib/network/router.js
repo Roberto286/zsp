601345b12f8794dff50af6180a2c9d8a8a75312e
roberto286 <roberto.saliola96@gmail.com>
2026-03-18 23:50:11 +0100

    fixed path variables retrieval

--- src/lib/network/router.js
+++ src/lib/network/router.js
@@ -1,46 +1,60 @@
 import * as url from "node:url";
 
 export class Router {
+  static routes = new Map();
 
-    static routes = new Map();
+  static register(path, method, handler, options) {
+    const parts = path.split("/").filter(Boolean);
+    const params = Router.extractParams(path);
+    const regex = new RegExp(
+      "^" +
+        parts
+          .map((part) => (part.startsWith(":") ? "([^/]+)" : part))
+          .join("/") +
+        "$",
+    );
 
-    static register(path, method, handler, options) {
-        const parts = path.split('/').filter(Boolean);
-        const params = Router.extractParams(path);
-        const regex = new RegExp('^' + parts.map(part =>
-            part.startsWith(':') ? '([^/]+)' : part
-        ).join('/') + '$');
+    Router.routes.set(`${method}:${path}`, {
+      method,
+      regex,
+      params,
+      handler,
+      path,
+      options,
+    });
+  }
 
-        Router.routes.set(`${method}:${path}`, { method, regex, params, handler, path, options });
-    }
-
-    static extractParams(path) {
-        const params = [];
-        const parts = path.split('/').filter(Boolean);
-        parts.forEach((part, index) => {
-            if(part.startsWith(':')) {
-                params.push({
-                    name: part.slice(1),
-                    index
-                });
-            }
+  static extractParams(path) {
+    const params = [];
+    const parts = path.split("/").filter(Boolean);
+    parts.forEach((part, index) => {
+      if (part.startsWith(":")) {
+        params.push({
+          name: part.slice(1),
+          index,
         });
-        return params;
-    }
+      }
+    });
+    return params;
+  }
 
-    static findRoute(req) {
-        const parsedUrl = url.parse(req.url, true);
-        const path = parsedUrl.pathname.substring(1);
-        const method = req.method;
+  static findRoute(req) {
+    const parsedUrl = url.parse(req.url, true);
+    const path = parsedUrl.pathname.substring(1);
+    const method = req.method;
 
-        for(const [_, route] of Router.routes) {
-            if(route.method === method) {
-                const match = path.match(route.regex);
-                if(match) {
-                    req.params = Router.extractParams(route.path, path);
-                    return route;
-                }
-            }
+    for (const [_, route] of Router.routes) {
+      if (route.method === method) {
+        const match = path.match(route.regex);
+        if (match) {
+          const params = Router.extractParams(route.path, path);
+          for (const param of params) {
+            req.params = req?.params || {};
+            req.params[param.name] = match[1];
+          }
+          return route;
         }
+      }
     }
+  }
 }
